739732ee3a4aea1e93b0b70a93e4277f9cced20d
Bruno123-n <hungriakaa@gmail.com>
2026-04-17 19:42:59 -0300

    git add.    git commit -m 'feat: exibe projetos dinamicamente com imagem e link'

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 const lista = document.getElementById('listaProjetos')
 
+//banco de dados
 const Projetos = [
     {
         nome: 'Login',
         descricao: 'Formulário com validação em JavaScript',
-        link: '#'
+        link: 'https://github.com/Bruno123-n/alinhando-codigos/tree/main/formulario2',
+        imagem: 'imagens/Login.png'
     },
     {
         nome: 'Lista de Tarefas',
         descricao: 'Aplicação com localStorage',
-        link: '#'
+        link: 'https://github.com/Bruno123-n/alinhando-codigos/tree/main/listadetarefas',
+        imagem: 'imagens/Lista-de-Tarefa.png'
     },
     {
         nome: 'Buscador de CEP',
         descricao: 'consumo de API com fetch',
-        link: '#'
+        link: 'https://github.com/Bruno123-n/alinhando-codigos/tree/main/buscarCep',
+        imagem: '/imagens/BuscadordeCEP.png'
+        
     }
 ]
 
@@ -23,6 +28,7 @@ Projetos.forEach(projeto => {
     div.classList.add('projeto')
 
     div.innerHTML = `
+        <img src='${projeto.imagem}' alt='${projeto.nome}'>
         <h3>${projeto.nome}</h3>
         <p>${projeto.descricao}</p>
         <a href="${projeto.link}" target="_blank">Ver projeto</a>
