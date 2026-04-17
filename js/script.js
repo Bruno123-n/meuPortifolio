b1c9a7fcbf857c6b178f5bb2f37856e9c82d36fb
Bruno123-n <hungriakaa@gmail.com>
2026-04-17 16:13:54 -0300

    git add.       git commit -m 'Objeto criado dentro de um Array'

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const lista = document.getElementById('listaProjetos')
+
+const Projetos = [
+    {
+        nome: 'Login',
+        descricao: 'Formulário com validação em JavaScript',
+        link: '#'
+    },
+    {
+        nome: 'Lista de Tarefas',
+        descricao: 'Aplicação com localStorage',
+        link: '#'
+    },
+    {
+        nome: 'Buscador de CEP',
+        descricao: 'consumo de API com fetch',
+        link: '#'
+    }
+]
+
+Projetos.forEach(projeto => {
+    const div = document.createElement('div')
+    div.classList.add('projeto')
+
+    
+})
